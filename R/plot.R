df84e7c55de9d54e5990a3ee6d0e017b286f1aab
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-05-28 20:30:30 -0700

    introduce plot labels
    helps show off what navigate_ordered does

--- R/plot.R
+++ R/plot.R
@@ -22,10 +22,17 @@ plot_pathway <- function(m, p, pca = FALSE) {
   p1 <- p$p1
   p2 <- p$p2
 
+  if (is.null(rownames(plot_m))) {
+    plot_labels <- plot_i
+  } else {
+    plot_labels <- rownames(plot_m)[plot_i]
+  }
+
   graphics::plot(plot_m, col = "gray", pch = 20)
   if (nrow(plot_line) > 1)
-    graphics:points(plot_line, pch = 4)
+    graphics::points(plot_line, pch = 4)
   graphics::points(plot_m[c(p1, p2),], col = "blue", pch = 15)
   graphics::points(plot_m[plot_i,], col = "red", pch = 16)
+  graphics::text(plot_m[plot_i,], labels = plot_labels, pos = 3)
   graphics::lines(plot_m[c(p1, plot_i, p2),])
 }
